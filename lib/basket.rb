53dd5fa7e388830807d33a553452ee84a9da2d6d
Aneta <wozniakaneta5@gmail.com>
2016-07-08 12:05:36 +0200

    initial commit

---
+++ lib/basket.rb
@@ -0,0 +1,27 @@
+class Basket
+	attr_reader :items
+	VAT = 1.23
+
+	def initialize
+		@items = []
+	end
+
+	def add(product, quantity)
+		items << { product: product, quantity: quantity }
+	end
+
+	def sum
+		total_price = 0
+		items.each do 
+			total_price += x[:price] 
+		end
+		"Total price to pay: #{total_price} zl."
+	end
+
+	def remove(product_id) 
+		#p = items.find {|item| item[:product].id == product_id } 
+		#items.delete(p)
+		items.delete_if{|item| item[:product].id == product_id}
+	end	
+end
+
---
+++ spec/unit/product_spec.rb
@@ -0,0 +1,11 @@
+require_relative "../../lib/product"
+
+RSpec.describe Product do
+	describe "self.new" do
+		it "able to create a new object" do
+			subject = Product.new(id: 1, name: "122", price: 123.33)
+
+			expect(subject.name).to eq("122")
+		end
+	end
+end
