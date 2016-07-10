17fd0a5866c412ef6bb531630a8a1ad2029ea3a3
Aneta <wozniakaneta5@gmail.com>
2016-07-10 10:04:09 +0200

    update warehouse - remove m&t

--- OOP/lib/warehouse.rb
+++ OOP/lib/warehouse.rb
@@ -1,4 +1,6 @@
 class Warehouse
+	attr_reader :products
+
 	def initialize(products: [])
 		@products = products
 	end
@@ -8,6 +10,15 @@ class Warehouse
 		@products.each_with_index do |p, index|
 			puts "#{index+1} | #{p.name} | #{p.price} | #{p.quantity}"
 		end
+	end
+
+	def add(id, name, price)
+		products << { id: id, name: name, price: price }
+	end
+
+	def remove(product_id)
+		p = products.find{|product| product.id == product_id}
+		products.delete(p)
 	end
 end
 			
